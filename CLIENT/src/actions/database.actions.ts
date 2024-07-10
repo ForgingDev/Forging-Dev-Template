d60a3b547b1153836faaf8e230f2797ceb2645f6
Dimoiu Bogdan <bogdan.dimoiu@itperspectives.ro>
2024-07-11 01:50:05 +0300

    Remove description

--- CLIENT/src/actions/database.actions.ts
+++ CLIENT/src/actions/database.actions.ts
@@ -8,7 +8,7 @@ export async function createUser(user: CreateUserModel): Promise<void> {
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ ...user, description: 'Delete later please' }),
+    body: JSON.stringify({ ...user }),
   });
 }
 
