7f71e923194cc1f3a3e3e20bf62db0532ead5b52
Dimoiu Bogdan <bogdan.dimoiu@itperspectives.ro>
2024-07-10 17:21:40 +0300

    Test db action from fe button

--- CLIENT/src/app/page.tsx
+++ CLIENT/src/app/page.tsx
@@ -1,7 +1,34 @@
+'use client';
+
+import { createUser } from '@/actions/database.actions';
+import { CreateUserModel } from '@/data/models/user.models';
 import { FC } from 'react';
 
 const Homepage: FC = () => {
-  return <></>;
+  return (
+    <div
+      onClick={async () => {
+        const user: CreateUserModel = {
+          id: '123',
+          email: ['pateu@gmail.com'],
+          phoneNumber: ['+40727892022'],
+          firstName: 'Pateu',
+          imageUrl: '',
+          lastName: 'Pateutz',
+          username: 'Pateutz24',
+        };
+
+        try {
+          const res = await createUser(user);
+
+          console.log(res);
+        } catch (error) {
+          console.error('Error creating user:', error);
+        }
+      }}>
+      Add user
+    </div>
+  );
 };
 
 export default Homepage;
