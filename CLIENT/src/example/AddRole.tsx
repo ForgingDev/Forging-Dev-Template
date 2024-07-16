b04ddd4a1a9b25ea1b3959bd12e88bdb536a2f9d
Dimoiu Bogdan <bogdan.dimoiu@itperspectives.ro>
2024-07-16 23:33:10 +0300

    Update

--- CLIENT/src/example/AddRole.tsx
+++ CLIENT/src/example/AddRole.tsx
@@ -3,6 +3,7 @@
 import useAddRoleAction from '@/actions/roles/addRole/useAddRole.action';
 import { Roles } from '@/data/models/roles.models';
 import useUserAccess from '@/hooks/useUserAccess';
+import { sendGTMEvent } from '@next/third-parties/google';
 import { FC } from 'react';
 
 const AddRole: FC = () => {
@@ -15,7 +16,11 @@ const AddRole: FC = () => {
 
   return (
     <button
-      onClick={() => handleAddRole(Roles.Admin)}
+      onClick={() => {
+        sendGTMEvent({ event: 'buttonClicked', value: 'xyz' });
+
+        handleAddRole(Roles.Admin);
+      }}
       disabled={addingRole}>
       {addingRole ? 'Adding role...' : 'Add new role'}
     </button>
